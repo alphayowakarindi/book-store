034b8051ba89c8e4a2fa93eade54c185058e2fea
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-30 17:56:47 +0300

    Add functionality to remove a book from the ui and redux store

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from './redux/books/books';
 
-function Book({ title, author }) {
+function Book({ id, title, author }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="book">
       <h4>{title}</h4>
       <span>{author}</span>
-      <button type="button">Remove</button>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>
+        Remove
+      </button>
     </div>
   );
 }
@@ -15,4 +21,5 @@ export default Book;
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
