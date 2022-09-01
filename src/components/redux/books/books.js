ac12ba9c97784c8a5e242e88803b64b91c7298e1
Alphy Gacheru <alphygacheru@gmail.com>
2022-09-01 14:30:46 +0300

    Initialize api const and create fetchBooksThunk

--- src/components/redux/books/books.js
+++ src/components/redux/books/books.js
@@ -1,6 +1,9 @@
 // Actions
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/books/GET_BOOKS';
+const api =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/XkIXtwdQN2As1Hyi39G5/books';
 
 // Reducer
 export default function booksReducer(
@@ -8,7 +11,7 @@ export default function booksReducer(
     { id: 1, title: 'River between', author: 'Ngugi wa Thiongo' },
     { id: 2, title: 'Born a Crime', author: 'Trevor Noah' },
   ],
-  action = {},
+  action = {}
 ) {
   switch (action.type) {
     case 'bookstore/books/ADD_BOOK':
@@ -20,6 +23,24 @@ export default function booksReducer(
       return state;
   }
 }
+
+// Redux thunks
+export const fetchBooksThunk = createAsyncThunk(GET_BOOKS, async () => {
+  const data = await fetch(api);
+  const response = await data.json();
+
+  const arr = [];
+  const keys = Object.keys(response);
+  keys.map((key) =>
+    arr.push({
+      item_id: key,
+      title: response[key][0].title,
+      author: response[key][0].author,
+      category: response[key][0].category,
+    })
+  );
+  return ([] = arr);
+});
 
 // Action Creators
 export function addBook(book) {
